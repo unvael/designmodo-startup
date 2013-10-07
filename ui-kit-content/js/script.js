e48ff16225c36cb7a37e30d0b8767c9b5bdf6f30
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-10-07 14:56:45 +0300

    add scripts

--- ui-kit-content/js/script.js
+++ ui-kit-content/js/script.js
@@ -38,6 +38,7 @@ startupKit.uiKitContent.content6 = function(){
 
 /* Content 7*/
 startupKit.uiKitContent.content7 = function(){
+	
 	(function(el) {
     	$('img:first-child', el).css('left', '-29.7%');
     	$(window).resize(function() {
@@ -58,6 +59,7 @@ startupKit.uiKitContent.content7 = function(){
 	  $('html').addClass('loaded');
 	  $(window).resize().scroll();
 	});
+	
 };
 
 /* Content 8*/
@@ -103,15 +105,60 @@ startupKit.uiKitContent.content15 = function(){
 /* Content 16*/
 startupKit.uiKitContent.content16 = function(){
 	
+	$('.content-16 .control-btn').on('click', function() {
+   		$.scrollTo($(this).closest('section').next(), {axis:'y', duration:500});
+    	return false;
+  	});
+  	
 };
 
 /* Content 17*/
 startupKit.uiKitContent.content17 = function(){
 	
+	// Carousel auto height
+  	$(window).resize(function() {
+    	$('#c-17_myCarousel').each(function() {
+      		var maxH = 0;
+     		$('.item', this).each(function() {
+        		var h = $(this).outerHeight();
+        		if (h > maxH) maxH = h;
+      		});
+      		$('#c-17_myCarousel .carousel-inner', this).css('height', maxH+'px');
+    	});
+  	});
+
+	// Carousel start
+	$('#c-17_myCarousel').carousel({interval: 4000});
+	
+	$(window).resize().scroll();
+
 };
 
 /* Content 18*/
 startupKit.uiKitContent.content18 = function(){
+
+	// Carousel auto height
+  	$(window).resize(function() {
+    	$('#c-18_myCarousel').each(function() {
+      		var maxH = 0;
+      		$('.item', this).each(function() {
+        		var h = $(this).outerHeight();
+        		if (h > maxH) maxH = h;
+      		});
+      		$('.carousel-inner', this).css('height', maxH+'px');
+    	});
+  	});
+
+  	$('#c-18_myCarousel').bind('slid', function() {
+    	$('.carousel-control', this).removeClass('disabled');
+    	if ($('.item.active', this).index() == 0) {
+      		$('.carousel-control.left', this).addClass('disabled');
+    	} else if ($('.item.active', this).index() == ($('.item', this).length - 1)) {
+      		$('.carousel-control.right', this).addClass('disabled');
+    	}
+  	});
+	
+	$(window).resize().scroll();
 	
 };
 

2c567ac1db0870b12025fb7853cc8e6354bcad08
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-10-15 17:17:51 +0300

    add header semples (17)

--- ui-kit-header/js/script.js
+++ ui-kit-header/js/script.js
@@ -223,6 +223,37 @@ startupKit.uiKitHeader.header16 = function(){
 /* Header 17*/
 startupKit.uiKitHeader.header17 = function(){
 	
+	startupKit.uiKitHeader._inFixedMode('.header-17');
+	var pt = PageTransitions();
+  pt.init('#h-17-pt-1');
+
+  $('.pt-controls .pt-indicators > *').on('click', function() {
+    if ($(this).hasClass('active')) return false;
+
+    var curPage = $(this).parent().children('.active').index();
+    var nextPage = $(this).index();
+    var ani = 44;
+    if (curPage < nextPage) {
+      ani = 45;
+    }
+
+    pt.gotoPage(ani, nextPage);
+    $(this).addClass('active').parent().children().not(this).removeClass('active');
+    return false;
+  });
+
+  $(window).resize(function() {
+    $('.header-17-sub .page-transitions').each(function() {
+      var maxH = 0;
+      $('.pt-page', this).css('height', 'auto').each(function() {
+        var h = $(this).outerHeight();
+        if (h > maxH) maxH = h;
+      }).css('height', maxH+'px');
+      $(this).css('height', maxH+'px');
+    });
+  });
+
+	
 };
 
 /* Header 18*/

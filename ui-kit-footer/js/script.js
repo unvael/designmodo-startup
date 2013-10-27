012259a993e152345b1ad6a00e98215794c1a25d
Alexander Nazarov <Alexander.Nazarov@dataart.com>
2013-10-27 14:18:36 +0200

    UI Kit Footer fixes

--- ui-kit-footer/js/script.js
+++ ui-kit-footer/js/script.js
@@ -100,5 +100,11 @@ startupKit.uiKitFooter.footer15 = function() {
                 });
             });
         }
+        // Focus state for append/prepend inputs
+        $('.input-prepend, .input-append').on('focus', 'input', function () {
+            $(this).closest('.control-group, form').addClass('focus');
+        }).on('blur', 'input', function () {
+            $(this).closest('.control-group, form').removeClass('focus');
+        });
     });
 })(jQuery); 

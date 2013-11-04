72f6a90668dd6cb7d6a6c10dea40eff5614bf70a
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-11-04 17:46:51 +0200

    add global js file

--- ui-kit-blog/js/script.js
+++ ui-kit-blog/js/script.js
@@ -1,63 +1,93 @@
+window.startupKit = window.startupKit || {};
+startupKit.uiKitBlog = startupKit.uiKitBlog || {};
+
+/* Blog 1*/
+startupKit.uiKitBlog.blog1 = function() {
+
+    $(window).resize(function() {
+        $('.page-transitions').each(function() {
+            var maxH = 0;
+            $('.pt-page', this).css('height', 'auto').each(function() {
+                var h = $(this).outerHeight();
+                if (h > maxH)
+                    maxH = h;
+            }).css('height', maxH + 'px');
+            $(this).css('height', maxH + 'px');
+        });
+    });
+
+    var pt1 = PageTransitions();
+    pt1.init($('#b1-pt-1'));
+
+    $('#b1-pt-1 .pt-control-prev').on('click', function() {
+        pt1.gotoPage(28, 'prev');
+        return false;
+    });
+
+    $('#b1-pt-1 .pt-control-next').on('click', function() {
+        pt1.gotoPage(29, 'next');
+        return false;
+    });
+
+};
+
+/* Blog 2*/
+startupKit.uiKitBlog.blog2 = function() {
+
+};
+/* Blog 3*/
+startupKit.uiKitBlog.blog3 = function() {
+
+};
+/* Blog 4*/
+startupKit.uiKitBlog.blog4 = function() {
+
+};
+/* Blog 5*/
+startupKit.uiKitBlog.blog5 = function() {
+
+    var pt2 = PageTransitions();
+    pt2.init($('#b5-pt-2'));
+
+    $('#b5-pt-2 .pt-control-prev').on('click', function() {
+        pt2.gotoPage(28, 'prev');
+        return false;
+    });
+
+    $('#b5-pt-2 .pt-control-next').on('click', function() {
+        pt2.gotoPage(29, 'next');
+        return false;
+    });
+
+};
 
 (function($) {
-$(function() {
+    $(function() {
+        
+        for (blog in startupKit.uiKitBlog) {
+            blogNumber = blog.slice(4);
+            if (jQuery('.blog-' + blogNumber).length != 0) {
+                startupKit.uiKitBlog[blog]();
+            };
+        }
 
-  if (/msie/i.test(navigator.userAgent)) {
-    $('img').each(function() {
-      $(this).css({
-        width: $(this).attr('width') + 'px',
-        height: 'auto'
-      });
+        if (/msie/i.test(navigator.userAgent)) {
+            $('img').each(function() {
+                $(this).css({
+                    width : $(this).attr('width') + 'px',
+                    height : 'auto'
+                });
+            });
+        }
+
+        // Focus state for append/prepend inputs
+        $('.input-prepend, .input-append').on('focus', 'input', function() {
+            $(this).closest('.control-group, form').addClass('focus');
+        }).on('blur', 'input', function() {
+            $(this).closest('.control-group, form').removeClass('focus');
+        });
+
+        // page-transitions
+
     });
-  }
-
-
-  // Focus state for append/prepend inputs
-  $('.input-prepend, .input-append').on('focus', 'input', function () {
-    $(this).closest('.control-group, form').addClass('focus');
-  }).on('blur', 'input', function () {
-    $(this).closest('.control-group, form').removeClass('focus');
-  });
-
-
-  // page-transitions
-  $(window).resize(function() {
-    $('.page-transitions').each(function() {
-      var maxH = 0;
-      $('.pt-page', this).css('height', 'auto').each(function() {
-        var h = $(this).outerHeight();
-        if (h > maxH) maxH = h;
-      }).css('height', maxH+'px');
-      $(this).css('height', maxH+'px');
-    });
-  });
-
-  var pt1 = PageTransitions();
-  pt1.init($('#pt-1'));
-
-  $('#pt-1 .pt-control-prev').on('click', function() {
-    pt1.gotoPage(28, 'prev');
-    return false;
-  });
-
-  $('#pt-1 .pt-control-next').on('click', function() {
-    pt1.gotoPage(29, 'next');
-    return false;
-  });
-
-
-  var pt2 = PageTransitions();
-  pt2.init($('#pt-2'));
-
-  $('#pt-2 .pt-control-prev').on('click', function() {
-    pt2.gotoPage(28, 'prev');
-    return false;
-  });
-
-  $('#pt-2 .pt-control-next').on('click', function() {
-    pt2.gotoPage(29, 'next');
-    return false;
-  });
-
-});
 })(jQuery);
--- ui-kit-crew/js/script.js
+++ ui-kit-crew/js/script.js
@@ -1,29 +1,31 @@
+window.startupKit = window.startupKit || {};
+startupKit.uiKitCrew = startupKit.uiKitCrew ||
+function() {
+    $('.member .photo img').each(function() {
+        $(this).hide().parent().css('background-image', 'url("' + this.src + '")');
+    });
+};
 
 (function($) {
-$(function() {
+    $(function() {
 
-  if (/msie/i.test(navigator.userAgent)) {
-    $('img').each(function() {
-      $(this).css({
-        width: $(this).attr('width') + 'px',
-        height: 'auto'
-      });
+        if (/msie/i.test(navigator.userAgent)) {
+            $('img').each(function() {
+                $(this).css({
+                    width : $(this).attr('width') + 'px',
+                    height : 'auto'
+                });
+            });
+        }
+
+        // Focus state for append/prepend inputs
+        $('.input-prepend, .input-append').on('focus', 'input', function() {
+            $(this).closest('.control-group, form').addClass('focus');
+        }).on('blur', 'input', function() {
+            $(this).closest('.control-group, form').removeClass('focus');
+        });
+
+       startupKit.uiKitCrew();
+
     });
-  }
-
-
-  // Focus state for append/prepend inputs
-  $('.input-prepend, .input-append').on('focus', 'input', function () {
-    $(this).closest('.control-group, form').addClass('focus');
-  }).on('blur', 'input', function () {
-    $(this).closest('.control-group, form').removeClass('focus');
-  });
-
-
-  // replace img to background-image
-  $('.member .photo img').each(function() {
-    $(this).hide().parent().css('background-image', 'url("' + this.src + '")');
-  });
-
-});
 })(jQuery);

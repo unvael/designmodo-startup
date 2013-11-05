0557f88a3a8bb1be0d908afdcac53aae0360bc4b
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-11-05 14:00:08 +0200

    add sample 9 and remove tpl-9

--- tpl-21new/js/script.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $(function() {
-        startupKit.uiKitHeader._inFixedMode(".header-custom-sub");
-        $(window).resize(function() {
-            $('.header-custom-sub').css('height', $(this).height() + 'px');
-        });
-    });
-})(jQuery);

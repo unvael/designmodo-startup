ea8a50207c0157d91ff575b0f20e04ac7da237bf
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-11-11 15:15:28 +0200

    developing manual

--- manual/js/script.js
+++ manual/js/script.js
@@ -12,6 +12,25 @@
             $(this).removeClass('active');    
             $(this).addClass('normal');
          });
+         
+         _locationChange = function(hash){
+             $('.content > div').hide();
+             if(hash==undefined||hash==null){hash = window.location.hash;}
+             if(hash==''||hash=="#"){hash='#main-page';}             
+             $(hash).fadeIn("slow");
+         };
+          _locationChange();
+         
+         if($('li.active', this).length==0){
+            $('li a[href='+window.location.hash+']').parent().addClass('active');
+         }
+         
+         $('li a, .brand', this).click(function(){
+            $('li').removeClass('active');
+            $(this).parent().addClass('active');             
+            _locationChange($(this).attr('href'));                        
+         });
+         
        };        
        manual.dockbar.init();
     });

c269ca3fcc1035205bd7039be143c10353cceff7
Vladimir Kanubrikov <vovank86@gmail.com>
2013-11-22 18:08:01 +0200

    manual new design

--- manual/js/script.js
+++ manual/js/script.js
@@ -1,27 +1,11 @@
 (function($) {
     $(function() {
 
-        startupKit.uiKitHeader._inFixedMode('.dockbar');
-
-        var dockbar = $('.dockbar');
-        dockbar.addClass('active');
-        dockbar.mouseover(function() {
-            $(this).removeClass('normal');
-            $(this).addClass('active');
-        });
-        dockbar.mouseout(function() {
-            $(this).removeClass('active');
-            $(this).addClass('normal');
-        });
-        $(window).scroll(function() {
-            $(dockbar).removeClass('active');
-            $(dockbar).addClass('normal');
-        });
-
         $('.manual > div').hide();
         var helpButton = $('.read-man');
         helpButton.each(function() {
             $(this).hover(function() {
+                $('body').toggleClass('hovered');
                 var id = $(this).attr('id');
                 var heightMask = 0;                 
                 if($('section.' + id).length!=0){
@@ -39,6 +23,10 @@
                 if($('footer.' + id).length!=0){
                     heightMask += $('footer.' + id).outerHeight();
                 }
+
+                if(heightMask > $(window).height()){
+                    $('.' + id + '.mask').addClass('big');
+                }
                 $('.' + id + '.mask').height(heightMask);
                 $('.' + id + '.mask').toggleClass('active');
             });
@@ -50,18 +38,18 @@
             var elemPos = 0;
             
             if($('.'+id+'-map').length!=0){
-                elemPos = $(this).position().top + 100;
+                elemPos = $(this).position().top + 110;
             }else{
                 elemPos = elem.position().top;
             }
             
             
-            $(window).scrollTop(elemPos -100);    
-            if($('.'+id+'-map').length!=0){                
-                elem.css('border-bottom', '3px solid red').css('border-left', '3px solid red').css('border-right', '3px solid red');
-            } else{
-                elem.css('border', '3px solid red');
-            }        
+            $(window).scrollTop(elemPos -110);
+//            if($('.'+id+'-map').length!=0){
+//                elem.css('border-bottom', '3px solid red').css('border-left', '3px solid red').css('border-right', '3px solid red');
+//            } else{
+//                elem.css('border', '3px solid red');
+//            }
             
             $('html').addClass('read-manual');            
             $('.manual .' + id).show();
@@ -71,7 +59,7 @@
         backButton.click(function() {
             var id = $(this).parent().attr('class');
             var elem = $('.mcontent > .'+id+ ':not(.mask)');
-            elem.css('border', '0 none');
+            //elem.css('border', '0 none');
             $('html').removeClass('read-manual');
             $('.manual > div').hide();
         });

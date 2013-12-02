092e3e16951c23f98c1104bbde042df2e73807bf
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-12-02 13:21:22 +0200

    replace animation effect into sample#1

--- common-files/js/startup-kit.js
+++ common-files/js/startup-kit.js
@@ -140,11 +140,11 @@ startupKit.uiKitHeader.header1 = function() {
     var pt = PageTransitions();
     pt.init('#pt-main');
     $('#pt-main .control-prev').on('click', function() {
-        pt.gotoPage(21, 'prev');
+        pt.gotoPage(5, 'prev');
         return false;
     });
     $('#pt-main .control-next').on('click', function() {
-        pt.gotoPage(22, 'next');
+        pt.gotoPage(6, 'next');
         return false;
     });
 

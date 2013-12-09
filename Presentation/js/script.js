59c6130609876fc9dd9578ca6e9097bfa99f4535
Alexander Nazarov <Alexander.Nazarov@dataart.com>
2013-12-09 21:26:25 +0200

    Presentation "Components and Samples"

--- Presentation/js/script.js
+++ Presentation/js/script.js
@@ -1,14 +1,56 @@
 $(document).ready(function() {    
         
         
-        var videobackground = new $.backgroundVideo($('#bgVideo'), {
-            "align" : "centerXY",
-            "path" : "video/",
-            "height":"auto",
-            "width":"100%",
-            "filename" : "preview",
-            "types" : ["mp4", "ogg", "webm"]
-        });
-   
+    var videobackground = new $.backgroundVideo($('#bgVideo'), {
+        "align" : "centerXY",
+        "path" : "video/",
+        "height":"auto",
+        "width":"100%",
+        "filename" : "preview",
+        "types" : ["mp4", "ogg", "webm"]
+    });
 
+
+    var dof = $('.presentation-componentsSamples-inner');
+	dof.mousemove(function(e){
+		var y = e.pageY - this.offsetTop;
+		var sHeight = dof.outerHeight();
+		
+		var offset = 0.2;
+		
+		var y = y - sHeight*offset;
+		var sHeight = sHeight - sHeight*offset*2;
+		
+		if (y >= sHeight) y = sHeight;
+		
+		var rounded = roundNum(((y/sHeight)*100)/5);
+		var hovered = Math.ceil(rounded);
+								
+		if (hovered == 0) hovered = 1;
+		
+		if (window.oldHovered != rounded){
+			dof.find('*').each(function( index ) {
+				index = index+1;
+				if (index >= hovered){
+					dof.find('.layer-'+index).css('opacity',1);
+				} else {
+					dof.find('.layer-'+index).css('opacity',0);
+				}
+				
+				if ((!$.browser.msie)&&(!$.browser.mozilla)){
+					if (hovered != rounded){
+						var halfOfWay = hovered - 1;
+						var backOpacity = roundNum(hovered-rounded);
+						dof.find('.layer-'+halfOfWay).css('opacity',backOpacity);
+					}
+				}
+			});
+			window.oldHovered = rounded;
+		}
+	});
 }); 
+
+function roundNum(num) {
+	num = Math.round(num*10)/10;
+	return num;
+}

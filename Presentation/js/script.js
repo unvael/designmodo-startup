59b894bd2e43910409606a057380e387d5dc2e0f
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-12-09 18:43:14 +0200

    add presentation

---
+++ Presentation/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {    
+        
+        
+        var videobackground = new $.backgroundVideo($('#bgVideo'), {
+            "align" : "centerXY",
+            "path" : "video/",
+            "height":"auto",
+            "width":"100%",
+            "filename" : "preview",
+            "types" : ["mp4", "ogg", "webm"]
+        });
+   
+
+}); 

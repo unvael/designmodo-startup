f0b2b1acc00127af14fc1f4f46b5473f70fc8f72
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-12-11 17:47:18 +0200

    presentation slider

--- Presentation/js/script.js
+++ Presentation/js/script.js
@@ -1,26 +1,24 @@
-$(document).ready(function () {
+$(document).ready(function() {
+
+    var iframe = $('#pPlayer')[0];
+    var player = $f(iframe);
+    player.addEvent('ready', function() {
+    });
+
+    function addEvent(element, eventName, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, callback, false);
+        } else {
+            element.attachEvent(eventName, callback, false);
+        }
+    }
 
 
-
-var iframe = $('#pPlayer')[0];
-var player = $f(iframe);
-	player.addEvent('ready', function() {});
-
-
-     function addEvent(element, eventName, callback) {
-                    if (element.addEventListener) {
-                        element.addEventListener(eventName, callback, false);
-                    }
-                    else {
-                        element.attachEvent(eventName, callback, false);
-                    }
-                }
-
-    $('#play').click(function () {
+    $('#play').click(function() {
         $('.presentation-mask').fadeIn('slow');
         $('.presentation-popup-video').fadeIn('slow');
         player.api('play')
-        $('.presentation-mask').click(function () {
+        $('.presentation-mask').click(function() {
             player.api('pause');
             $('.presentation-popup-video').fadeOut('slow');
             $('.presentation-mask').fadeOut('slow');
@@ -29,18 +27,105 @@ var player = $f(iframe);
     });
 
     var videobackground = new $.backgroundVideo($('#bgVideo'), {
-        "align": "centerXY",
-        "path": "video/",
-        "height": "auto",
-        "width": "100%",
-        "filename": "preview",
-        "types": ["mp4", "ogg", "webm"]
+        "align" : "centerXY",
+        "path" : "video/",
+        "height" : "auto",
+        "width" : "100%",
+        "filename" : "preview",
+        "types" : ["mp4", "ogg", "webm"]
     });
 
+    var presentationScrollBlock = function(id) {
+        var sbWidth = 0;
+        var scrollBlock = $('#' + id);
+        var displayWidth = $(window).width();
 
+        var topPosition = scrollBlock.position().top;
+        var bottomPosition = topPosition + scrollBlock.outerHeight();
+
+        $('.container .row > *', scrollBlock).each(function() {
+            sbWidth += $(this).width();
+        });
+
+        var parts = sbWidth / displayWidth;
+        if (parts % 1 > 0)
+            parts = parts - (parts % 1) + 1;
+
+        $('.presentation-controls ul', scrollBlock).html('');
+        for (var i = 1; i <= parts; i++) {
+            $('.presentation-controls ul', scrollBlock).append('<li data-part=' + i + '></li>');
+        }
+
+        var partWidth = sbWidth / parts;
+        partWidth = partWidth - (partWidth % 1);
+
+        var startPoint = function(i) {
+            if (i == parts) {
+                return sbWidth - displayWidth;
+            } else {
+                return (i - 1) * partWidth;
+            }
+
+        };
+        function indScroll() {
+            for (var i = 1; i <= parts; i++) {
+
+                if (scrollBlock.scrollLeft() == startPoint(i)) {
+                    $('li', scrollBlock).removeClass('active');
+                    $('li[data-part="' + i + '"]', scrollBlock).addClass('active');
+                    $('.presentation-controls', scrollBlock).css('left', scrollBlock.scrollLeft());
+                }
+            }
+        };
+        indScroll();
+        scrollBlock.on('scroll', function() {
+            indScroll();
+        });
+
+        $('li', scrollBlock).click(function() {
+            var part = $(this).attr('data-part');
+            scrollBlock.animate({
+                scrollLeft : startPoint(part)
+            }, 800);
+
+        });
+
+        $(window).on("mousewheel", function(e, d) {
+            console.log( test = e);
+            if ($(window).width() > 900) {
+                if ($(window).scrollTop() >= topPosition && $(window).scrollTop() <= bottomPosition) {
+                    $(window).scrollTop(topPosition);
+                    e.preventDefault();                    
+                    if (d < 0) {
+                        if ($('li.active', scrollBlock).attr('data-part') == $('li:last-child', scrollBlock).attr('data-part')) {
+                            $(window).scrollTop(bottomPosition+1);
+                        } else {
+                            $(window).scrollTop(topPosition);
+                            $('li.active', scrollBlock).next().click();
+                        }
+                    }else{
+                         if ($('li.active', scrollBlock).attr('data-part') == $('li:first-child', scrollBlock).attr('data-part')) {
+                            $(window).scrollTop(topPosition-1);
+                        } else {
+                            $(window).scrollTop(topPosition);
+                            $('li.active', scrollBlock).prev().click();
+                        }
+                    }
+                }
+            } else {
+                return e;
+            }
+        });
+
+    };
+
+    presentationScrollBlock('scrollBlock-1');
+    $(window).resize(function() {
+        presentationScrollBlock('scrollBlock-1');
+    });
 
     var dof = $('.presentation-componentsSamples-inner');
-    dof.mousemove(function (e) {
+    dof.mousemove(function(e) {
         var y = e.pageY - $(this).offset().top;
         var sHeight = dof.outerHeight();
 
@@ -49,15 +134,17 @@ var player = $f(iframe);
         var y = y - sHeight * offset;
         var sHeight = sHeight - sHeight * offset * 2;
 
-        if (y >= sHeight) y = sHeight;
+        if (y >= sHeight)
+            y = sHeight;
 
         var rounded = roundNum(((y / sHeight) * 100) / 5);
         var hovered = Math.ceil(rounded);
 
-        if (hovered == 0) hovered = 1;
+        if (hovered == 0)
+            hovered = 1;
 
         if (window.oldHovered != rounded) {
-            dof.find('*').each(function (index) {
+            dof.find('*').each(function(index) {
                 index = index + 1;
                 if (index >= hovered) {
                     dof.find('.layer-' + index).css('opacity', 1);
@@ -84,37 +171,37 @@ var player = $f(iframe);
         var sliderItems = $('li', sliderList);
         var sliderAmount = sliderItems.length;
 
-        ctrlTrigger.hover(function () {
-          sliderList.addClass('titles-on');
-        }, function () {
-          sliderList.removeClass('titles-on');
+        ctrlTrigger.hover(function() {
+            sliderList.addClass('titles-on');
+        }, function() {
+            sliderList.removeClass('titles-on');
         });
         var scrollers = $(".scroller").mCustomScrollbar({
-          scrollButtons:{
-            enable: Boolean
-          },
-          autoDraggerLength: true,
-          advanced:{
-            updateOnBrowserResize: true,
-            updateOnContentResize: true
-          },
-          contentTouchScroll:true
+            scrollButtons : {
+                enable : Boolean
+            },
+            autoDraggerLength : true,
+            advanced : {
+                updateOnBrowserResize : true,
+                updateOnContentResize : true
+            },
+            contentTouchScroll : true
         });
 
         var bottomSubSlider = $('.additional-slider .sub-slider:first').bxSlider({
-          'controls': false,
-          'pagerCustom': '.additional-slider .sub-slider-ctrl',
-          'mode': 'fade',
-          'easing': 'ease-in-out',
-          'adaptiveHeight': true
+            'controls' : false,
+            'pagerCustom' : '.additional-slider .sub-slider-ctrl',
+            'mode' : 'fade',
+            'easing' : 'ease-in-out',
+            'adaptiveHeight' : true
         });
 
         var bottomSlider = $('.additional-slider .bxslider:first').bxSlider({
-          'controls': false,
-          'pagerCustom': '.additional-slider .container-slider-controls ul',
-          'mode': 'horizontal',
-          'easing': 'ease-in-out',
-          'adaptiveHeight': true
+            'controls' : false,
+            'pagerCustom' : '.additional-slider .container-slider-controls ul',
+            'mode' : 'horizontal',
+            'easing' : 'ease-in-out',
+            'adaptiveHeight' : true
         });
 
     }

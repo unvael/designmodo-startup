2a30d641cefc7036297e7f9ff49a8944ac3e2be1
Alexander Nazarov <Alexander.Nazarov@dataart.com>
2013-12-12 21:57:50 +0200

    Sample 25

---
+++ sample-25/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    var iframe = $('#pPlayer')[0];
+    var player = $f(iframe);
+    player.addEvent('ready', function() {});
+
+    function addEvent(element, eventName, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, callback, false);
+        } else {
+            element.attachEvent(eventName, callback, false);
+        }
+    }
+
+
+    $('#play').click(function(evt) {
+        evt.preventDefault();
+        $('.mask').fadeIn('slow');
+        $('.popup-video').fadeIn('slow');
+        player.api('play')
+        $('.mask').click(function() {
+            player.api('pause');
+            $('.popup-video').fadeOut('slow');
+            $('.mask').fadeOut('slow');
+
+        });
+    });
+});

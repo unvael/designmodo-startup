abdf0aea61270a427eee5f4ebff359fa1e688cf0
Vladimir Kanubrikov <vovank86@gmail.com>
2013-12-15 17:25:58 +0200

    manual menu

--- manual/js/script.js
+++ manual/js/script.js
@@ -10,6 +10,7 @@
         });
 
 
+
         $('html').click(function (e) {
             // console.warn('element clicked = ',$(e.target).attr('class'), '\n parent element = ', $(e.target).parents().attr('class'));
             if (!$(e.target).hasClass('menu-btn') && !$(e.target).hasClass('colapsed-menu') && !$(e.target).parents().hasClass('colapsed-menu') && !$(e.target).parents().hasClass('menu-btn')) {

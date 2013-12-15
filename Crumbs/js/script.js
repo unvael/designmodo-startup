094d48bf1b972d36906a0fcb03272cf6bc70a348
Vladimir Kanubrikov <vovank86@gmail.com>
2013-12-16 01:08:40 +0200

    Crumbs and HipstaFood

--- Crumbs/js/script.js
+++ Crumbs/js/script.js
@@ -1,7 +1,38 @@
 (function($) {
-    $(function() {    
- 
- $(window).resize(function() {
+    $(function() {
+
+        var iframe = $('#cPlayer')[0];
+        var player = $f(iframe);
+        player.addEvent('ready', function() {
+        });
+
+        function addEvent(element, eventName, callback) {
+            if (element.addEventListener) {
+                element.addEventListener(eventName, callback, false);
+            } else {
+                element.attachEvent(eventName, callback, false);
+            }
+        }
+
+
+        $('#play').click(function(e) {
+            e.preventDefault();
+            $('.crumbs-mask').fadeIn('slow');
+            $('.crumbs-popup-video').fadeIn('slow');
+            player.api('play')
+            $('.crumbs-mask').click(function() {
+                player.api('pause');
+                $('.crumbs-popup-video').fadeOut('slow');
+                $('.crumbs-mask').fadeOut('slow');
+
+            });
+        });
+
+
+
+
+
+        $(window).resize(function() {
             var sH = $(window).height();
             $('section.header-10-sub').css('height', (sH - $('header').outerHeight()) + 'px');
             $('section:not(.header-10-sub):not(.content-13, .logos, .projects-3, .sc2-custom-section-3, .sc2-custom-section-1)').css('height', sH + 'px');

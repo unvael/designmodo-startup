e43d2fd2c786c078965aa43bfeca4545b6c3503e
Svetlana Pugach <Svetlana.Pugach@dataart.com>
2014-02-19 15:49:22 +0200

    SF-159

--- samples/sample-25/js/script.js
+++ samples/sample-25/js/script.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    function isAppleDevice() {
+        return (
+            (navigator.userAgent.toLowerCase().indexOf("ipad") > -1) ||
+                (navigator.userAgent.toLowerCase().indexOf("iphone") > -1) ||
+                (navigator.userAgent.toLowerCase().indexOf("ipod") > -1)
+            );
+    }
 
     var iframe = $('#pPlayer')[0];
     var player = $f(iframe);
@@ -26,7 +33,11 @@ $(document).ready(function() {
         });
     });
 
-    $('.content-23').each(function() {
-        $(this).parallax('50%', 0.3, true);
-    });
+
+    if (isAppleDevice() == false) {
+        $('.content-23').each(function () {
+            $(this).css("background-attachment", "fixed")
+            $(this).parallax('50%', 0.2, true);
+        });
+    }
 });

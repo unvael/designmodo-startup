81cb140b121284c4b5e9907fb9f7ebe90918d028
Svetlana Pugach <Svetlana.Pugach@dataart.com>
2014-02-20 16:53:07 +0200

    SF-279

--- samples/sample-24/js/script.js
+++ samples/sample-24/js/script.js
@@ -1,7 +1,5 @@
 
 $(window).resize(function() {
-
     var sH = $(window).height();
-    $('section.header-14-sub').css('height', sH + 'px');
-
+    $('section.header-14-sub').css('min-height', sH + 'px');
 });
